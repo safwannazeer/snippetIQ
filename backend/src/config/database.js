9833f07e22a99d0655f3363804df1d002821f223
safwannazeer <safwannazeer2@gmail.com>
2026-04-11 23:44:47 +0530

    minor database changes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGO_URI || process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
   if (!MONGODB_URI) {
